bb8f7ee6efa52ffaab54338ae4d4085fea0fa546
sukitha jayasingha <sukithaj@gmail.com>
2020-02-22 08:45:55 +0530

    formatting added

--- config/default.js
+++ config/default.js
@@ -1,56 +1,51 @@
 module.exports = {
-  "DB": {
-    "Type":"postgres",
-    "User":"duo",
-    "Password":"DuoS123",
-    "Port":5432,
-    "Host":"104.236.231.11",
-    "Database":"duo"
+  DB: {
+    Type: "postgres",
+    User: "duo",
+    Password: "DuoS123",
+    Port: 5432,
+    Host: "ec2-3-15-222-85.us-east-2.compute.amazonaws.com",
+    Database: "duo"
   },
-   "Redis":
-  {
-    "mode":"sentinel",//instance, cluster, sentinel
-    "ip": "45.55.142.207",
-    "port": 6389,
-    "user": "duo",
-    "password": "DuoS123",
-    "sentinels":{
-      "hosts": "138.197.90.92,45.55.205.92,138.197.90.92",
-      "port":16389,
-      "name":"redis-cluster"
-    }
-
-  },
-
-  "Security":
-  {
-
-    "ip" : "45.55.142.207",
-    "port": 6389,
-    "user": "duo",
-    "password": "DuoS123",
-    "mode":"sentinel",//instance, cluster, sentinel
-    "sentinels":{
-      "hosts": "138.197.90.92,45.55.205.92,138.197.90.92",
-      "port":16389,
-      "name":"redis-cluster"
+  Redis: {
+    mode: "instance", //instance, cluster, sentinel
+    ip: "ec2-18-189-192-150.us-east-2.compute.amazonaws.com",
+    port: 6379,
+    user: "duo",
+    password: "DuoS123",
+    sentinels: {
+      hosts: "138.197.90.92,45.55.205.92,138.197.90.92",
+      port: 16389,
+      name: "redis-cluster"
     }
   },
 
-
-  "RabbitMQ": {
-    "ip":"45.55.142.207",
-    "port":"5672",
-    "user": "admin",
-    "password": "admin",
-    "vhost":'/'
+  Security: {
+    ip: "ec2-18-189-192-150.us-east-2.compute.amazonaws.com",
+    port: 6379,
+    user: "duo",
+    password: "DuoS123",
+    mode: "instance", //instance, cluster, sentinel
+    sentinels: {
+      hosts: "138.197.90.92,45.55.205.92,138.197.90.92",
+      port: 16389,
+      name: "redis-cluster"
+    }
   },
 
-  "Host":{
-    "Ip":"0.0.0.0",
-    "Port":"8823",
-    "Version":"1.0.0.0"
+  RabbitMQ: {
+    ip: "ec2-18-189-192-150.us-east-2.compute.amazonaws.com",
+    port: "5672",
+    user: "duo",
+    password: "DuoS123",
+    vhost: "/"
   },
 
-  "evtConsumeType": 'amqp'
+  Host: {
+    Ip: "0.0.0.0",
+    Port: "8823",
+    Version: "1.0.0.0"
+  },
+
+  evtConsumeType: "amqp"
 };
