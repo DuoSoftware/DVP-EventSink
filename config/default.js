11c8373dc2510ecfd673ed7426bde3c1b863b5fd
sukitha jayasingha <sukithaj@gmail.com>
2020-02-22 14:27:11 +0530

    amqp subscribe fix added

--- config/default.js
+++ config/default.js
@@ -1,64 +1,60 @@
 module.exports = {
-    "DB": {
-        "Type":"postgres",
-        "User":"duo",
-        "Password":"DuoS123",
-        "Port":5432,
-        "Host":"104.236.231.11",
-        "Database":"duo"
-    },
-    "Redis":
-        {
-            "mode":"sentinel",//instance, cluster, sentinel
-            "ip": "45.55.142.207",
-            "port": 6389,
-            "user": "duo",
-            "password": "DuoS123",
-            "sentinels":{
-                "hosts": "138.197.90.92,45.55.205.92,138.197.90.92",
-                "port":16389,
-                "name":"redis-cluster"
-            }
-
-        },
-
-    "Security":
-        {
-
-            "ip" : "45.55.142.207",
-            "port": 6389,
-            "user": "duo",
-            "password": "DuoS123",
-            "mode":"sentinel",//instance, cluster, sentinel
-            "sentinels":{
-                "hosts": "138.197.90.92,45.55.205.92,138.197.90.92",
-                "port":16389,
-                "name":"redis-cluster"
-            }
-        },
-
-
-    "RabbitMQ": {
-        "ip":"45.55.142.207",
-        "port":"5672",
-        "user": "admin",
-        "password": "admin",
-        "vhost":'/'
-    },
-
-    "Host":{
-        "Ip":"0.0.0.0",
-        "Port":"8823",
-        "Version":"1.0.0.0"
-    },
-
-  "EventTrigger": {
-        "ip": 'eventtriggerservice.app1.veery.cloud',
-        "port": 3645,
-        "version": '1.0.0.0'
+  DB: {
+    Type: "postgres",
+    User: "duo",
+    Password: "DuoS123",
+    Port: 5432,
+    Host: "104.236.231.11",
+    Database: "duo"
+  },
+  Redis: {
+    mode: "sentinel", //instance, cluster, sentinel
+    ip: "45.55.142.207",
+    port: 6389,
+    user: "duo",
+    password: "DuoS123",
+    sentinels: {
+      hosts: "138.197.90.92,45.55.205.92,138.197.90.92",
+      port: 16389,
+      name: "redis-cluster"
+    }
   },
 
-  "evtConsumeType": 'amqp',
-  "triggerToIntegrations": 'true',
-  "Token": "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJzdWtpdGhhIiwianRpIjoiYWEzOGRmZWYtNDFhOC00MWUyLTgwMzktOTJjZTY0YjM4ZDFmIiwic3ViIjoiNTZhOWU3NTlmYjA3MTkwN2EwMDAwMDAxMjVkOWU4MGI1YzdjNGY5ODQ2NmY5MjExNzk2ZWJmNDMiLCJleHAiOjE5MDIzODExMTgsInRlbmFudCI6LTEsImNvbXBhbnkiOi0xLCJzY29wZSI6W3sicmVzb3VyY2UiOiJhbGwiLCJhY3Rpb25zIjoiYWxsIn1dLCJpYXQiOjE0NzAzODExMTh9.Gmlu00Uj66Fzts-w6qEwNUz46XYGzE8wHUhAJOFtiRo"
+  Security: {
+    ip: "45.55.142.207",
+    port: 6389,
+    user: "duo",
+    password: "DuoS123",
+    mode: "sentinel", //instance, cluster, sentinel
+    sentinels: {
+      hosts: "138.197.90.92,45.55.205.92,138.197.90.92",
+      port: 16389,
+      name: "redis-cluster"
+    }
+  },
+
+  RabbitMQ: {
+    ip: "45.55.142.207",
+    port: "5672",
+    user: "admin",
+    password: "admin",
+    vhost: "/"
+  },
+
+  Host: {
+    Ip: "0.0.0.0",
+    Port: "8823",
+    Version: "1.0.0.0"
+  },
+
+  EventTrigger: {
+    ip: "eventtriggerservice.app1.veery.cloud",
+    port: 3645,
+    version: "1.0.0.0"
+  },
+
+  evtConsumeType: "amqp",
+  triggerToIntegrations: "true",
+  Token:
+    "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJzdWtpdGhhIiwianRpIjoiYWEzOGRmZWYtNDFhOC00MWUyLTgwMzktOTJjZTY0YjM4ZDFmIiwic3ViIjoiNTZhOWU3NTlmYjA3MTkwN2EwMDAwMDAxMjVkOWU4MGI1YzdjNGY5ODQ2NmY5MjExNzk2ZWJmNDMiLCJleHAiOjE5MDIzODExMTgsInRlbmFudCI6LTEsImNvbXBhbnkiOi0xLCJzY29wZSI6W3sicmVzb3VyY2UiOiJhbGwiLCJhY3Rpb25zIjoiYWxsIn1dLCJpYXQiOjE0NzAzODExMTh9.Gmlu00Uj66Fzts-w6qEwNUz46XYGzE8wHUhAJOFtiRo"
 };
