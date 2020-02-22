c7d813d18e8c8a40922b7450998671bbd2e53f41
sukitha jayasingha <sukithaj@gmail.com>
2020-02-22 14:22:14 +0530

    amqp subscription fixed

--- app.js
+++ app.js
@@ -3,267 +3,280 @@
  * test
  */
 
-var redisHandler = require('./RedisHandler.js');
-var dbBackendHandler = require('./DbBackendHandler.js');
-var dbModel = require('dvp-dbmodels');
-var config = require('config');
-var nodeUuid = require('node-uuid');
-var logger = require('dvp-common/LogHandler/CommonLogHandler.js').logger;
-var util = require('util');
-var amqp = require('amqp');
-var externalApiHandler = require('./ExternalApiAccess.js');
+var redisHandler = require("./RedisHandler.js");
+var dbBackendHandler = require("./DbBackendHandler.js");
+var dbModel = require("dvp-dbmodels");
+var config = require("config");
+var nodeUuid = require("node-uuid");
+var logger = require("dvp-common/LogHandler/CommonLogHandler.js").logger;
+var util = require("util");
+var amqp = require("amqp");
+var externalApiHandler = require("./ExternalApiAccess.js");
+let readyCount = 0;
 
+var saveOnDB = function(
+  sessionId,
+  evtName,
+  companyId,
+  tenantId,
+  evtClass,
+  evtType,
+  evtCategory,
+  evtTime,
+  evtData,
+  evtParams,
+  bUnit
+) {
+  var evt = dbModel.DVPEvent.build({
+    SessionId: sessionId,
+    EventName: evtName,
+    CompanyId: companyId,
+    TenantId: tenantId,
+    EventClass: evtClass,
+    EventType: evtType,
+    EventCategory: evtCategory,
+    EventTime: evtTime,
+    EventData: evtData,
+    EventParams: evtParams,
+    BusinessUnit: bUnit
+  });
 
-var saveOnDB = function(sessionId, evtName, companyId, tenantId, evtClass, evtType, evtCategory, evtTime, evtData, evtParams, bUnit)
-{
-    var evt = dbModel.DVPEvent.build({
-        SessionId: sessionId,
-        EventName: evtName,
-        CompanyId: companyId,
-        TenantId: tenantId,
-        EventClass: evtClass,
-        EventType: evtType,
-        EventCategory: evtCategory,
-        EventTime: evtTime,
-        EventData: evtData,
-        EventParams: evtParams,
-        BusinessUnit: bUnit
-
-    });
-
-    dbBackendHandler.AddEventData(evt, function(err, result)
-    {
-        if(err)
-        {
-            logger.error('[DVP-EventService.DVPEVENTS] - [%s] - dbBackendHandler.AddEventData threw an exception', reqId, err);
-        }
-    })
+  dbBackendHandler.AddEventData(evt, function(err, result) {
+    if (err) {
+      logger.error(
+        "[DVP-EventService.DVPEVENTS] - [%s] - dbBackendHandler.AddEventData threw an exception",
+        reqId,
+        err
+      );
+    }
+  });
 };
 
-if(config.evtConsumeType === 'amqp')
-{
-    var amqpConState = 'CLOSED';
+if (config.evtConsumeType === "amqp") {
+  var amqpConState = "CLOSED";
 
-    // var connection = amqp.createConnection({ host: rmqIp, port: rmqPort, login: rmqUser, password: rmqPassword});
+  // var connection = amqp.createConnection({ host: rmqIp, port: rmqPort, login: rmqUser, password: rmqPassword});
 
-    var ips = [];
-    if(config.RabbitMQ.ip) {
-        ips = config.RabbitMQ.ip.split(",");
+  var ips = [];
+  if (config.RabbitMQ.ip) {
+    ips = config.RabbitMQ.ip.split(",");
+  }
+
+  var connection = amqp.createConnection(
+    {
+      //url: queueHost,
+      host: ips,
+      port: config.RabbitMQ.port,
+      login: config.RabbitMQ.user,
+      password: config.RabbitMQ.password,
+      vhost: config.RabbitMQ.vhost,
+      noDelay: true,
+      heartbeat: 10
+    },
+    {
+      reconnect: true,
+      reconnectBackoffStrategy: "linear",
+      reconnectExponentialLimit: 120000,
+      reconnectBackoffTime: 1000
     }
+  );
 
+  //logger.debug('[DVP-EventMonitor.handler] - [%s] - AMQP Creating connection ' + rmqIp + ' ' + rmqPort + ' ' + rmqUser + ' ' + rmqPassword);
 
-    var connection = amqp.createConnection({
-        //url: queueHost,
-        host: ips,
-        port: config.RabbitMQ.port,
-        login: config.RabbitMQ.user,
-        password: config.RabbitMQ.password,
-        vhost: config.RabbitMQ.vhost,
-        noDelay: true,
-        heartbeat:10
-    }, {
-        reconnect: true,
-        reconnectBackoffStrategy: 'linear',
-        reconnectExponentialLimit: 120000,
-        reconnectBackoffTime: 1000
-    });
+  connection.on("connect", function() {
+    amqpConState = "CONNECTED";
+    logger.debug(
+      "[DVP-EventService.AMQPConnection] - [%s] - AMQP Connection CONNECTED"
+    );
+  });
 
-    //logger.debug('[DVP-EventMonitor.handler] - [%s] - AMQP Creating connection ' + rmqIp + ' ' + rmqPort + ' ' + rmqUser + ' ' + rmqPassword);
+  connection.on("heartbeat", function() {
+    logger.debug("RabbitMQ HeartBeat");
+  });
 
-    connection.on('connect', function()
-    {
-        amqpConState = 'CONNECTED';
-        logger.debug('[DVP-EventService.AMQPConnection] - [%s] - AMQP Connection CONNECTED');
-    });
+  connection.on("ready", function() {
+    amqpConState = "READY";
 
-    connection.on('ready', function()
-    {
-        amqpConState = 'READY';
+    logger.debug(
+      "[DVP-EventService.AMQPConnection] - [%s] - AMQP Connection READY"
+    );
+    readyCount += 1;
+    if (readyCount === 1)
+      connection.queue(
+        "DVPEVENTS",
+        { durable: true, autoDelete: false },
+        function(q) {
+          q.bind("#");
 
-        logger.debug('[DVP-EventService.AMQPConnection] - [%s] - AMQP Connection READY');
+          // Receive messages
+          q.subscribe(function(message) {
+            // Print messages to stdout
+            var reqId = nodeUuid.v1();
+            var evtObj = message;
 
-        connection.queue('DVPEVENTS', {durable: true, autoDelete: false}, function (q) {
-            q.bind('#');
+            var evtClass = evtObj["EventClass"];
+            var evtType = evtObj["EventType"];
+            var evtCategory = evtObj["EventCategory"];
+            var evtTime = evtObj["EventTime"];
+            var evtName = evtObj["EventName"];
+            var evtData = evtObj["EventData"];
+            var evtParams = evtObj["EventParams"];
+            var companyId = evtObj["CompanyId"];
+            var tenantId = evtObj["TenantId"];
+            var sessionId = evtObj["SessionId"];
+            var bUnit = evtObj["BusinessUnit"];
+            var evtSpecificData = evtObj["EventSpecificData"];
 
-            // Receive messages
-            q.subscribe(function (message) {
-                // Print messages to stdout
-                var reqId = nodeUuid.v1();
-                var evtObj = message;
+            if (!companyId) {
+              companyId = -1;
+            }
 
-                var evtClass = evtObj['EventClass'];
-                var evtType = evtObj['EventType'];
-                var evtCategory = evtObj['EventCategory'];
-                var evtTime = evtObj['EventTime'];
-                var evtName = evtObj['EventName'];
-                var evtData = evtObj['EventData'];
-                var evtParams = evtObj['EventParams'];
-                var companyId = evtObj['CompanyId'];
-                var tenantId = evtObj['TenantId'];
-                var sessionId = evtObj['SessionId'];
-                var bUnit = evtObj['BusinessUnit'];
-                var evtSpecificData = evtObj['EventSpecificData'];
+            if (!tenantId) {
+              tenantId = -1;
+            }
 
-                if(!companyId)
-                {
-                    companyId = -1;
-                }
+            if (evtParams && util.isObject(evtParams)) {
+              evtParams = JSON.stringify(evtParams);
+            }
 
-                if(!tenantId)
-                {
-                    tenantId = -1;
-                }
+            if (evtSpecificData && util.isObject(evtSpecificData)) {
+              evtSpecificData = JSON.stringify(evtSpecificData);
+            }
 
+            //saveOnDB(sessionId, evtName, companyId, tenantId, evtClass, evtType, evtCategory, evtTime, evtData, evtParams, bUnit);
 
-                if(evtParams && util.isObject(evtParams)){
-
-                    evtParams = JSON.stringify(evtParams);
-                }
-
-                if(evtSpecificData && util.isObject(evtSpecificData)){
-
-                    evtSpecificData = JSON.stringify(evtSpecificData);
-                }
-
-
-                //saveOnDB(sessionId, evtName, companyId, tenantId, evtClass, evtType, evtCategory, evtTime, evtData, evtParams, bUnit);
-
-                var evt = dbModel.DVPEvent.build({
-                    SessionId: sessionId,
-                    EventName: evtName,
-                    CompanyId: companyId,
-                    TenantId: tenantId,
-                    EventClass: evtClass,
-                    EventType: evtType,
-                    EventCategory: evtCategory,
-                    EventTime: evtTime,
-                    EventData: evtData,
-                    EventParams: evtParams,
-                    BusinessUnit: bUnit
-
-                });
-
-                console.log('TriggerToIntegration : ' + config.triggerToIntegrations);
-
-                //if ((evtClass === 'CALL' || evtClass === 'TICKET' || evtClass === 'AGENT' || evtName === 'ards-added') && evtSpecificData)
-                if ((evtClass === 'CALL' || evtClass === 'TICKET' || evtClass === 'AGENT' || evtName === 'ards-added') && config.triggerToIntegrations === 'true' && evtSpecificData)
-                {
-                    var evtype = "";
-                    if(evtClass === 'CALL' || evtName === 'ards-added')
-                    {
-                        evtype = "CALL";
-                    }
-                    else if(evtClass === 'TICKET')
-                    {
-                        evtype = "TICKET";
-                    }
-                    else if(evtClass === 'AGENT')
-                    {
-                        evtype = "AGENT";
-                    }
-                    //Call API
-                    externalApiHandler.EventTrigger(companyId, tenantId, evtype, evtSpecificData);
-                }
-                else
-                {
-                    console.log('NOT SENDING TRIGGER');
-                }
-
-                evt
-                    .save()
-                    .then(function (rsp)
-                    {
-                        message = null;
-                        evtObj = null;
-                        evt = null;
-                        rsp = null;
-
-                    }).catch(function(err)
-                    {
-                        logger.error('[DVP-EventService.DVPEVENTS] - dbBackendHandler.AddEventData threw an exception', err);
-                    });
-
-
+            var evt = dbModel.DVPEvent.build({
+              SessionId: sessionId,
+              EventName: evtName,
+              CompanyId: companyId,
+              TenantId: tenantId,
+              EventClass: evtClass,
+              EventType: evtType,
+              EventCategory: evtCategory,
+              EventTime: evtTime,
+              EventData: evtData,
+              EventParams: evtParams,
+              BusinessUnit: bUnit
             });
 
-        });
-    });
+            console.log(
+              "TriggerToIntegration : " + config.triggerToIntegrations
+            );
 
-    connection.on('error', function(e)
-    {
-        logger.error('[DVP-EventMonitor.handler] - AMQP Connection ERROR', e);
-        amqpConState = 'CLOSE';
-    });
-
-}
-else
-{
-    redisHandler.RedisSubscribe('SYS:MONITORING:DVPEVENTS');
-
-    redisHandler.redisClient.on('message', function(channel, message)
-    {
-        if(channel && channel === 'SYS:MONITORING:DVPEVENTS')
-        {
-            var reqId = nodeUuid.v1();
-
-            logger.debug('[DVP-EventService.DVPEVENTS] - Event Received - Params - message : %s, appId : %s', message);
-
-            if(message)
-            {
-                var evtObj = JSON.parse(message);
-
-                var evtClass = evtObj['EventClass'];
-                var evtType = evtObj['EventType'];
-                var evtCategory = evtObj['EventCategory'];
-                var evtTime = evtObj['EventTime'];
-                var evtName = evtObj['EventName'];
-                var evtData = evtObj['EventData'];
-                var evtParams = evtObj['EventParams'];
-                var companyId = evtObj['CompanyId'];
-                var tenantId = evtObj['TenantId'];
-                var sessionId = evtObj['SessionId'];
-
-                if(!companyId)
-                {
-                    companyId = -1;
-                }
-
-                if(!tenantId)
-                {
-                    tenantId = -1;
-                }
-
-
-                if(evtParams && util.isObject(evtParams)){
-
-                    evtParams = JSON.stringify(evtParams);
-                }
-
-                var evt = dbModel.DVPEvent.build({
-                    SessionId: sessionId,
-                    EventName: evtName,
-                    CompanyId: companyId,
-                    TenantId: tenantId,
-                    EventClass: evtClass,
-                    EventType: evtType,
-                    EventCategory: evtCategory,
-                    EventTime: evtTime,
-                    EventData: evtData,
-                    EventParams: evtParams
-
-                });
-
-                dbBackendHandler.AddEventData(evt, function(err, result)
-                {
-                    if(err)
-                    {
-                        logger.error('[DVP-EventService.DVPEVENTS] - [%s] - dbBackendHandler.AddEventData threw an exception', reqId, err);
-                    }
-                })
+            //if ((evtClass === 'CALL' || evtClass === 'TICKET' || evtClass === 'AGENT' || evtName === 'ards-added') && evtSpecificData)
+            if (
+              (evtClass === "CALL" ||
+                evtClass === "TICKET" ||
+                evtClass === "AGENT" ||
+                evtName === "ards-added") &&
+              config.triggerToIntegrations === "true" &&
+              evtSpecificData
+            ) {
+              var evtype = "";
+              if (evtClass === "CALL" || evtName === "ards-added") {
+                evtype = "CALL";
+              } else if (evtClass === "TICKET") {
+                evtype = "TICKET";
+              } else if (evtClass === "AGENT") {
+                evtype = "AGENT";
+              }
+              //Call API
+              externalApiHandler.EventTrigger(
+                companyId,
+                tenantId,
+                evtype,
+                evtSpecificData
+              );
+            } else {
+              console.log("NOT SENDING TRIGGER");
             }
+
+            evt
+              .save()
+              .then(function(rsp) {
+                message = null;
+                evtObj = null;
+                evt = null;
+                rsp = null;
+              })
+              .catch(function(err) {
+                logger.error(
+                  "[DVP-EventService.DVPEVENTS] - dbBackendHandler.AddEventData threw an exception",
+                  err
+                );
+              });
+          });
+        }
+      );
+  });
+
+  connection.on("error", function(e) {
+    logger.error("[DVP-EventMonitor.handler] - AMQP Connection ERROR", e);
+    amqpConState = "CLOSE";
+  });
+} else {
+  redisHandler.RedisSubscribe("SYS:MONITORING:DVPEVENTS");
+
+  redisHandler.redisClient.on("message", function(channel, message) {
+    if (channel && channel === "SYS:MONITORING:DVPEVENTS") {
+      var reqId = nodeUuid.v1();
+
+      logger.debug(
+        "[DVP-EventService.DVPEVENTS] - Event Received - Params - message : %s, appId : %s",
+        message
+      );
+
+      if (message) {
+        var evtObj = JSON.parse(message);
+
+        var evtClass = evtObj["EventClass"];
+        var evtType = evtObj["EventType"];
+        var evtCategory = evtObj["EventCategory"];
+        var evtTime = evtObj["EventTime"];
+        var evtName = evtObj["EventName"];
+        var evtData = evtObj["EventData"];
+        var evtParams = evtObj["EventParams"];
+        var companyId = evtObj["CompanyId"];
+        var tenantId = evtObj["TenantId"];
+        var sessionId = evtObj["SessionId"];
+
+        if (!companyId) {
+          companyId = -1;
         }
 
-    });
+        if (!tenantId) {
+          tenantId = -1;
+        }
+
+        if (evtParams && util.isObject(evtParams)) {
+          evtParams = JSON.stringify(evtParams);
+        }
+
+        var evt = dbModel.DVPEvent.build({
+          SessionId: sessionId,
+          EventName: evtName,
+          CompanyId: companyId,
+          TenantId: tenantId,
+          EventClass: evtClass,
+          EventType: evtType,
+          EventCategory: evtCategory,
+          EventTime: evtTime,
+          EventData: evtData,
+          EventParams: evtParams
+        });
+
+        dbBackendHandler.AddEventData(evt, function(err, result) {
+          if (err) {
+            logger.error(
+              "[DVP-EventService.DVPEVENTS] - [%s] - dbBackendHandler.AddEventData threw an exception",
+              reqId,
+              err
+            );
+          }
+        });
+      }
+    }
+  });
 }
 
 process.stdin.resume();
